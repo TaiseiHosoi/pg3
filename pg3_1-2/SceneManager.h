79adee1c31ab2ab0dbba09c7e7769a8ee9b2c1a1
suga <k021g1264@g.neec.ac.jp>
2023-01-23 11:08:49 +0900

    未開の外部シンボル

---
+++ pg3_1-2/SceneManager.h
@@ -0,0 +1,20 @@
+#pragma once
+#include"Singleton.h"
+
+class SceneManager :public Singleton<SceneManager>
+{
+	friend Singleton<SceneManager>;
+private:
+	// コンストラクタ,デストラクタ
+	SceneManager();
+	~SceneManager();
+public:
+	
+	void ChangeScene(int sceneNum);
+
+	void TitleScene();
+	void NewGameScene();
+	void GamePlayScene();
+	void GameClearScene();
+};
+
---
+++ pg3_1-2/Singleton.h
@@ -0,0 +1,13 @@
+#pragma once
+template <class T>
+
+class Singleton {
+public:
+    static T& GetInstance() {
+        static T instance;
+        return instance;
+    }
+protected:
+    Singleton() {};
+    virtual ~Singleton() {};
+};
